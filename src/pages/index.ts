1da580d6ba2b4ddf4c4a5a994cb2f6daee9ad1ab
닮 <dhdpekfa1@daum.net>
2025-08-12 20:15:49 +0900

    chore: 폴더 구조

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
-export { LoginPage } from './LoginPage';
-export { VerificationPage } from './VerificationPage';
-export { ConfirmationPage } from './ConfirmationPage';
+// 화면 많아질 경우 디렉토리 별로 분리 필요
+export { LoginPage } from './login/LoginPage';
+export { VerificationPage } from './verification/VerificationPage';
+export { ConfirmationPage } from './confirmation/ConfirmationPage';
 export { NotFoundPage } from './NotFoundPage';

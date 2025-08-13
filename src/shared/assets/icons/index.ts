c8e70a2605190d268b4071c27711ba2ab4691077
닮 <dhdpekfa1@daum.net>
2025-08-13 17:34:52 +0900

    chore: 로고, 아이콘 추가

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import ChevronDownSvg from './chevron-down.svg';
+import ChevronLeftSvg from './chevron-left.svg';
+import ChevronRightSvg from './chevron-right.svg';
+import ChevronUpSvg from './chevron-up.svg';
+
+export { ChevronDownSvg, ChevronLeftSvg, ChevronRightSvg, ChevronUpSvg };

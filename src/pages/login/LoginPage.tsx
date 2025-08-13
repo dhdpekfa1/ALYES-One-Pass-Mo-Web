28bb12fa6d8ad608df349829a628ef64ca4bd7e9
닮 <dhdpekfa1@daum.net>
2025-08-13 18:54:41 +0900

    feat: 공통 컴포넌트

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,21 @@
+import { Dropdown } from '@/shared/ui';
+import { useState } from 'react';
+
 export const LoginPage = () => {
+  const [value, setValue] = useState('');
   return (
     <div>
-      <h1 className='text-2xl font-bold'>출석 사전 확인/회원 인증</h1>
+      <div className='mr-200 mt-10 ml-10'>
+        <Dropdown
+          items={['A', 'B', 'C']}
+          selectedItem={value}
+          placeholder='선택'
+          onSelect={v => setValue(v)}
+          hasError={false}
+          errorMessage='필수 선택입니다.'
+          description='선택해주세요.'
+        />
+      </div>
     </div>
   );
 };
---
+++ src/shared/ui/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './Button';
+export { LabelInput } from './LabelInput';
+export { Dropdown } from './Dropdown';
